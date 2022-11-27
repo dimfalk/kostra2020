0c7ea69a815fa3acb1bbb7e629604fd97fc74b8b
falk-env <dimitri.falk1@gmail.com>
2022-11-27 14:09:32 +0100

    doc(data): reproducible pre-processing

--- data-raw/kostra_dwd_2010r.R
+++ data-raw/kostra_dwd_2010r.R
@@ -1,20 +1,38 @@
 ## code to prepare `kostra_dwd_2010r` dataset goes here
 
-files <- list.files(pattern = "*.shp", recursive = TRUE)
+source <- "https://opendata.dwd.de/climate_environment/CDC/grids_germany/return_periods/precipitation/KOSTRA/KOSTRA_DWD_2010R/gis/"
 
-d <- stringr::str_sub(files, start = 53, end = 56)
+d <- c(5, 10, 15, 20, 30, 45, 60, 90, 120, 180, 240, 360, 540, 720, 1080, 1440, 2880, 4320)
+
+d_pad <- stringr::str_pad(d, width = 4, pad = "0")
+
+files <- paste0("GIS_KOSTRA-DWD-2010R_D", d_pad,".zip")
+
+urls <- paste0(source, files)
+
+for (i in 1:length(files)) {
+
+  download.file(urls[i], files[i])
+
+  unzip(files[i])
+
+  unlink(files[i])
+}
+
+files_shp <- list.files(pattern = "*.shp", recursive = TRUE)
+
+kostra_dwd_2010r <- list()
 
 for (i in 1:length(d)) {
 
-  ds <- paste0("d", d[i])
+  ds <- paste0("D", d_pad[i])
 
-  assign(ds, paste0("GIS_KOSTRA-DWD-2010R_", ds, "/StatRR_KOSTRA-DWD-2010R_", ds, ".shp") |> sf::read_sf())
+  # temporary fix r-cmd-check on ubuntu-latest due to GDAL 3.0.2?
+  assign(ds, files_shp[i] |> sf::read_sf() |> sf::st_set_crs("epsg:3034"))
+
+  kostra_dwd_2010r[[ds]] <- get(ds)
 }
 
-kostra_dwd_2010r <- list(
-  "D0005" = d0005, "D0010" = d0010, "D0015" = d0015, "D0020" = d0020, "D0030" = d0030, "D0045" = d0045,
-  "D0060" = d0060, "D0090" = d0090, "D0120" = d0120, "D0180" = d0180, "D0240" = d0240, "D0360" = d0360,
-  "D0540" = d0540, "D0720" = d0720, "D1080" = d1080, "D1440" = d1440, "D2880" = d2880, "D4320" = d4320
-)
-
 usethis::use_data(kostra_dwd_2010r, overwrite = TRUE)
+
+unlink(files |> stringr::str_sub(1, -5), recursive = TRUE)
--- data-raw/osm_plz_centroids.R
+++ data-raw/osm_plz_centroids.R
@@ -1,8 +1,22 @@
 ## code to prepare `osm_plz_centroids` dataset goes here
 
-osm_plz_centroids <- sf::read_sf("plz-5stellig.shp") |>
+file <- "plz-5stellig.shp.zip"
+
+source <- paste0("https://downloads.suche-postleitzahl.org/v2/public/", file)
+
+download.file(source, file)
+
+unzip(file, exdir = file |> stringr::str_sub(1, -9))
+
+unlink(file)
+
+shp <- list.files(pattern = "plz-5stellig.shp", full.names = TRUE, recursive = TRUE)
+
+osm_plz_centroids <- sf::read_sf(shp) |>
   dplyr::select("plz") |>
   sf::st_centroid() |>
   sf::st_transform("epsg:4326")
 
 usethis::use_data(osm_plz_centroids, overwrite = TRUE)
+
+unlink(file |> stringr::str_sub(1, -9), recursive = TRUE)
