7cbba26bbf95912dd7bc65d586df17ed1a818905
falk-env <dimitri.falk1@gmail.com>
2023-01-10 16:54:10 +0100

    test(as_*): units

--- tests/testthat/test-as_yield.R
+++ tests/testthat/test-as_yield.R
@@ -1,8 +1,17 @@
-test_that("Function working as intended.", {
+test_that("Function working as intended with numeric.", {
 
   expect_equal(as_yield(x = 25.3, d = 5), units::as_units(843.3, "l s-1 ha-1"))
 
   expect_equal(as_yield(x = 45.7, d = 60), units::as_units(126.9, "l s-1 ha-1"))
 
   expect_equal(as_yield(x = 120.5, d = 1440), units::as_units(13.9, "l s-1 ha-1"))
+})
+
+test_that("Function working as intended with units", {
+
+  expect_equal(as_yield(x = units::as_units(25.3, "mm"), d = units::as_units(5, "min")), units::as_units(843.3, "l s-1 ha-1"))
+
+  expect_equal(as_yield(x = units::as_units(45.7, "mm"), d = units::as_units(60, "min")), units::as_units(126.9, "l s-1 ha-1"))
+
+  expect_equal(as_yield(x = units::as_units(120.5, "mm"), d = units::as_units(1440, "min")), units::as_units(13.9, "l s-1 ha-1"))
 })
--- tests/testthat/test-get_returnp.R
+++ tests/testthat/test-get_returnp.R
@@ -6,6 +6,8 @@ test_that("Getting return periods for DWD-KOSTRA-2020 works.", {
 
   expect_equal(get_returnp(kostra_ref, hn = 75.3, d = 1440), units::as_units(c(30, 50), "a"))
 
+  expect_equal(get_returnp(kostra_ref, hn = 90.0, d = 1440), units::as_units(c(50, 100), "a"))
+
   expect_equal(get_returnp(kostra_ref, hn = 96.3, d = 1440), units::as_units(c(100, Inf), "a"))
 })
 
@@ -17,6 +19,8 @@ test_that("Getting interpolated return periods for DWD-KOSTRA-2020 works.", {
 
   expect_equal(get_returnp(kostra_ref, hn = 75.3, d = 1440, interpolate = TRUE), units::as_units(37.3, "a"))
 
+  expect_equal(get_returnp(kostra_ref, hn = 90.0, d = 1440, interpolate = TRUE), units::as_units(96.5, "a"))
+
   expect_error(get_returnp(kostra_ref, hn = 96.3, d = 1440, interpolate = TRUE))
 })
 
@@ -27,6 +31,8 @@ test_that("Getting return periods for stats from DWA-A 531 works.", {
   expect_equal(get_returnp(stats_ref, hn = 40.7, d = 1440), units::as_units(c(2, 2), "a"))
 
   expect_equal(get_returnp(stats_ref, hn = 72.3, d = 1440), units::as_units(c(33, 50), "a"))
+
+  expect_equal(get_returnp(stats_ref, hn = 81.0, d = 1440), units::as_units(c(50, 100), "a"))
 
   expect_equal(get_returnp(stats_ref, hn = 86.3, d = 1440), units::as_units(c(100, Inf), "a"))
 })
