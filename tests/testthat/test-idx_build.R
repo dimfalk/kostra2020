8b145f43d3e898b96f78f34ece51e24f1d280339
falk-env <dimitri.falk1@gmail.com>
2023-01-12 20:56:13 +0100

    test(idx_build): extend

--- tests/testthat/test-idx_build.R
+++ tests/testthat/test-idx_build.R
@@ -2,5 +2,9 @@ test_that("Building of 'INDEX_RC' out of X and Y information works.", {
 
   expect_equal(idx_build(col = 125, row = 49), "49125")
 
-  expect_equal(idx_build(row = 10, col = 47), "10047")
+  expect_equal(idx_build(col = 1, row = 0), "1")
+
+  expect_equal(idx_build(col = 34, row = 36), "36034")
+
+  expect_equal(idx_build(col = 5, row = 102), "102005")
 })
